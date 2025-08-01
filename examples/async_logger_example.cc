8253f4b2f472f1cdb072cb7851502e320d5e9725
HuRuilizhen <huruilizhen@gmail.com>
2025-08-01 20:38:51 +0800

    fix(platform): revise out flag name to enabel run on windows

--- examples/async_logger_example.cc
+++ examples/async_logger_example.cc
@@ -4,9 +4,9 @@ int main(int argc, char *argv[]) {
   AsyncLogger::Config config;
   config.filename = "test.log";
   config.level = AsyncLogger::Level::Info;
-  config.flag = AsyncLogger::OutstreamFlag::stdout |
-                AsyncLogger::OutstreamFlag::stderr |
-                AsyncLogger::OutstreamFlag::file;
+  config.flag = AsyncLogger::OutstreamFlag::out_stdout |
+                AsyncLogger::OutstreamFlag::out_stderr |
+                AsyncLogger::OutstreamFlag::out_file;
   AsyncLogger::Logger::init(config);
   AsyncLogger::Logger::info("hello from info log!");
   AsyncLogger::Logger::shutdown();
