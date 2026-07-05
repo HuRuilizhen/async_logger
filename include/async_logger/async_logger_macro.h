3a2c5a439cd7fa4f3b71e9ca99ce0eae12d0c16f
HuRuilizhen <huruilizhen@gmail.com>
2026-07-06 04:33:30 +0800

    fix(macros): avoid msvc preprocessor dependency

--- include/async_logger/async_logger_macro.h
+++ include/async_logger/async_logger_macro.h
@@ -1,8 +1,25 @@
 #pragma once
 
 #include <format>
+#include <string>
+#include <string_view>
+#include <utility>
 
 #include "async_logger/async_logger.h"
+
+namespace AsyncLogger::Detail {
+
+inline std::string formatLogMessage(std::string_view msg) {
+  return std::string(msg);
+}
+
+template <typename... Args>
+inline std::string formatLogMessage(std::format_string<Args...> fmt,
+                                    Args&&... args) {
+  return std::format(fmt, std::forward<Args>(args)...);
+}
+
+}  // namespace AsyncLogger::Detail
 
 #define LOG_DEBUG(msg) AsyncLogger::Logger::debug(msg)
 #define LOG_INFO(msg) AsyncLogger::Logger::info(msg)
@@ -10,13 +27,13 @@
 #define LOG_ERROR(msg) AsyncLogger::Logger::error(msg)
 #define LOG_FATAL(msg) AsyncLogger::Logger::fatal(msg)
 
-#define LOGF_DEBUG(fmt, ...) \
-  AsyncLogger::Logger::debug(std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
-#define LOGF_INFO(fmt, ...) \
-  AsyncLogger::Logger::info(std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
-#define LOGF_WARN(fmt, ...) \
-  AsyncLogger::Logger::warn(std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
-#define LOGF_ERROR(fmt, ...) \
-  AsyncLogger::Logger::error(std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
-#define LOGF_FATAL(fmt, ...) \
-  AsyncLogger::Logger::fatal(std::format(fmt __VA_OPT__(, ) __VA_ARGS__))
+#define LOGF_DEBUG(...) \
+  AsyncLogger::Logger::debug(AsyncLogger::Detail::formatLogMessage(__VA_ARGS__))
+#define LOGF_INFO(...) \
+  AsyncLogger::Logger::info(AsyncLogger::Detail::formatLogMessage(__VA_ARGS__))
+#define LOGF_WARN(...) \
+  AsyncLogger::Logger::warn(AsyncLogger::Detail::formatLogMessage(__VA_ARGS__))
+#define LOGF_ERROR(...) \
+  AsyncLogger::Logger::error(AsyncLogger::Detail::formatLogMessage(__VA_ARGS__))
+#define LOGF_FATAL(...) \
+  AsyncLogger::Logger::fatal(AsyncLogger::Detail::formatLogMessage(__VA_ARGS__))
