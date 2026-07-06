2e60ae5c75201ccafe30fbe8fa5b13de093b579f
HuRuilizhen <huruilizhen@gmail.com>
2026-07-07 01:43:51 +0800

    ci: add clang-format check

--- examples/async_logger_example.cc
+++ examples/async_logger_example.cc
@@ -1,6 +1,6 @@
 #include "async_logger/async_logger.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   AsyncLogger::Config config;
   config.filename = "test.log";
   config.level = AsyncLogger::Level::Debug;
